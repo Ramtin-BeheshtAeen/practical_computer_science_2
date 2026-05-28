5043b13808c030cef1401bdd5abb23e863bf2143
RamtinBeheshtAeen <ramtinba145822@gmail.com>
2026-05-28 19:01:56 +0200

    practise sort insertation

---
+++ 07_Sortieren/src/de/uni_bremen/pi2/SortPractise1.java
@@ -0,0 +1,10 @@
+package de.uni_bremen.pi2;
+
+public class SortPractise1 {
+    public static <T extends Comparable<T>> void insertationSort(final T[] a){
+        //We start from one, every left side is already sorted/.
+        for (int i = 1; i < a.length; i++) {
+
+        }
+    }
+}
